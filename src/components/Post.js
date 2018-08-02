2c6d6cff77b6a230011de69ba5aee5bd2c3de705
Alexandra Ash <alexandraleah@gmail.com>
2018-08-02 17:52:00 -0400

    set up firebase

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,29 +1,58 @@
 import React, { Component } from 'react';
+import fire from '../fire';
 export default class Post extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = { messages: [] };
+    // this.handleSubmit = this.handleSubmit.bind(this);
   }
-  handleSubmit(event) {
-    event.preventDefault();
-    alert('you clicked the button');
+  componentWillMount() {
+    /* Create reference to messages in Firebase Database */
+    let messagesRef = fire
+      .database()
+      .ref('messages')
+      .orderByKey()
+      .limitToLast(100);
+    messagesRef.on('child_added', snapshot => {
+      /* Update React state when message is added at Firebase Database */
+      let message = { text: snapshot.val(), id: snapshot.key };
+      this.setState({ messages: [message].concat(this.state.messages) });
+    });
   }
 
-  handleSubmit(event) {
+  addMessage(event) {
     event.preventDefault();
-    alert('you clicked the button');
+    fire
+      .database()
+      .ref('messages')
+      .push(this.inputEl.value);
+    this.inputEl.value = '';
   }
+
+  // handleSubmit(event) {
+  //   event.preventDefault();
+  //   alert('you clicked the button');
+  // }
 
   render() {
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.addMessage.bind(this)}>
+          <input type="text" ref={el => (this.inputEl = el)} />
+          <input type="submit" />
+          <ul>
+            {/* Render the list of messages */
+            this.state.messages.map(message => (
+              <li key={message.id}>{message.text}</li>
+            ))}
+          </ul>
+        </form>
+        {/* <form onSubmit={this.handleSubmit}>
           <label htmlFor="photo">Take a photo</label>
           <input name="photo" type="file" accept="image/*" capture />
           <button type="submit">Upload</button>
         </form>
-        <img src="" />
+        <img src="" /> */}
       </div>
     );
   }
---
+++ src/fire.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+var config = {
+  apiKey: 'AIzaSyB9fXFRJIQnXFP_8NhZXgeGxEs5UgYYdW8',
+  authDomain: 'trash-to-treasur-1533175223809.firebaseapp.com',
+  databaseURL: 'https://trash-to-treasur-1533175223809.firebaseio.com',
+  projectId: 'trash-to-treasur-1533175223809',
+  storageBucket: 'trash-to-treasur-1533175223809.appspot.com',
+  messagingSenderId: '607714836865',
+};
+var fire = firebase.initializeApp(config);
+export default fire;
