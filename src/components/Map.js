fb64e115d2fd63d9b99a8acd1e02484978fee69c
Alexandra Ash <alexandraleah@gmail.com>
2018-08-03 20:44:00 -0400

    fix(database): trying to get data from the database asynchronously

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import MapContainer from './MapContainer';
 import Post from './Post';
+import firebase from '../fire';
+const database = firebase.database();
 
 export default class Map extends Component {
   constructor(props) {
     super(props);
-    this.state = { lat: -34.397, lng: 150.644 };
+    this.state = { lat: -34.397, lng: 150.644, treasures: {} };
   }
-  componentDidMount() {
+
+  async componentDidMount() {
+    /* Create reference to messages in Firebase Database */
+    let treasuresRef = database.ref('treasures');
+    treasuresRef.on('child_added', snapshot => {
+      /* Update React state when message is added at Firebase Database */
+      let treasure = { item: snapshot.val(), id: snapshot.key };
+      this.setState({ treasures: { ...this.state, treasure } });
+    });
     if (navigator && navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(pos => {
         const coords = pos.coords;
@@ -17,6 +27,7 @@ export default class Map extends Component {
         });
       });
     }
+    console.log(this.state);
   }
   render() {
     return (
