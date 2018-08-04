43022d8fc5dca25d5c663b4e0e5e4f3779f44489
Alexandra Ash <alexandraleah@gmail.com>
2018-08-04 10:45:37 -0400

    feature(layout): header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {
-  BrowserRouter as Router,
-  withRouter,
-  Route,
-  Switch,
-  Redirect,
-} from 'react-router-dom';
+import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
 import GoogleMap from './components/Map';
 import Post from './components/Post';
 
 export default function App(props) {
   return (
-    <Router>
+    <div>
+      <header>
+        <h1>Trash to Treasure</h1>
+      </header>
       <Switch>
         <Route exact path="/post" component={Post} />
         <Route path="/" component={GoogleMap} />
       </Switch>
-    </Router>
+    </div>
   );
 }
