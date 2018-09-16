fad2e72cbd26edf565090d8eb3c163a4b911f0bc
Alexandra Ash <alexandraleah@gmail.com>
2018-09-16 15:50:46 -0400

    updating

--- src/fire.js
+++ src/fire.js
@@ -1,15 +1,3 @@
 let firebase = require('firebase');
-// import 'firebase/auth';
-var config = {
-  apiKey: 'AIzaSyB9fXFRJIQnXFP_8NhZXgeGxEs5UgYYdW8',
-  authDomain: 'trash-to-treasur-1533175223809.firebaseapp.com',
-  databaseURL: 'https://trash-to-treasur-1533175223809.firebaseio.com',
-  projectId: 'trash-to-treasur-1533175223809',
-  storageBucket: 'trash-to-treasur-1533175223809.appspot.com',
-  messagingSenderId: '607714836865',
-};
-var fire = firebase.initializeApp(config);
+var fire = firebase.initializeApp();
 export default fire;
-
-// const auth = firebase.auth();
-// export { auth };
