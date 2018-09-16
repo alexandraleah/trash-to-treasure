47b25b853f1755a67ae35ae3f7963bdc164ae1e2
Alexandra Ash <alexandraleah@gmail.com>
2018-09-16 19:22:38 -0400

    update

--- src/components/MainMap.js
+++ src/components/MainMap.js
@@ -66,7 +66,7 @@ class MainMap extends Component {
       <div style={{ height: '90vh', width: '100%' }}>
         <GoogleMapReact
           bootstrapURLKeys={{
-            key: 'AIzaSyBK0aygWZBhZEQik-vEGakhVGnx0acVArA',
+            key: 'AIzaSyDU8bwFuCeqgGL0_AihNBNnh4AhX-DPvUw',
           }}
           defaultCenter={this.props.center}
           center={this.state.center}
--- src/geoLocationFunctions.js
+++ src/geoLocationFunctions.js
@@ -36,7 +36,7 @@ export let getUserPosition = async function() {
 export let lookUpAddress = async function(lat, long) {
   const latlng = lat.toString() + ',' + long.toString();
   const response = await axios.get(
-    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latlng}&key=AIzaSyAks3EMzDbX5oksronTU8511s0PzswB-go
+    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latlng}&key=AIzaSyDU8bwFuCeqgGL0_AihNBNnh4AhX-DPvUw
     `
   );
   //this api returns many possible human readable versions of the address. We grab the first one, which for these purposes is enough.
