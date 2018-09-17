82e7e0bba1628328e36db5b60d874cee96216ede
Alexandra Ash <alexandraleah@gmail.com>
2018-09-16 20:16:47 -0400

    removed map for testing

--- src/fire.js
+++ src/fire.js
@@ -1,3 +1,4 @@
 let firebase = require('firebase');
 var fire = firebase.initializeApp();
 export default fire;
+//maybe this is the problem? although doesn't seem to correspond with the error I am getting
